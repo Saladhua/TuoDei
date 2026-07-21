2520e38cdee5f3688ca822a7ced57f2a6e9d97e6
李 海川 <lihaichuan@shdcsoft.com>
2026-07-21 14:46:49 +0800

    新增: 暂估应付单_提交更新付款计划行号.py - 提交时更新付款计划FENTRYID/FSEQ

---
+++ 益讯机械/暂估应付单_提交更新付款计划行号.py
@@ -0,0 +1,43 @@
+import clr
+clr.AddReference('System')
+clr.AddReference('System.Data')
+clr.AddReference('Kingdee.BOS')
+clr.AddReference('Kingdee.BOS.DataEntity')
+clr.AddReference('Kingdee.BOS.Core')
+clr.AddReference('Kingdee.BOS.App')
+clr.AddReference('Kingdee.BOS.ServiceHelper')
+from Kingdee.BOS import *
+from Kingdee.BOS.Core import *
+from Kingdee.BOS.Core.Bill import *
+from Kingdee.BOS.Core.DynamicForm.PlugIn import *
+from Kingdee.BOS.Core.DynamicForm.PlugIn.Args import *
+from System import *
+from System.Data import *
+from Kingdee.BOS.ServiceHelper import *
+from Kingdee.BOS.Orm.DataEntity import *
+
+
+def BeforeDoOperation(e):
+    opCode = e.Operation.FormOperation.Operation.ToUpperInvariant()
+    if opCode != "SUBMIT":
+        return
+
+    bill = this.View.Model.DataObject
+
+    acctType = str(bill["FSETACCOUNTTYPE"]) if bill["FSETACCOUNTTYPE"] is not None else ""
+    if acctType != "2":
+        return
+
+    payPlan = bill["AP_PAYABLEPLAN"]
+    if payPlan is None or payPlan.Count == 0:
+        return
+
+    entries = bill["AP_PAYABLEENTRY"]
+    if entries is None or entries.Count == 0:
+        return
+
+    billId = Convert.ToInt64(bill["Id"])
+    firstEntryId = Convert.ToInt64(entries[0]["Id"])
+
+    sql = "UPDATE T_AP_PAYABLEPLAN SET FENTRYID={0},FSEQ='1' WHERE FID={1} AND (FENTRYID IS NULL OR FENTRYID=0)".format(firstEntryId, billId)
+    DBServiceHelper.ExecuteDataSet(this.View.Context, sql)
