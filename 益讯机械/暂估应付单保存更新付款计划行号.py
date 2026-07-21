8c2c20753f0b5fe77fbbb44889d721b283ad621e
李 海川 <lihaichuan@shdcsoft.com>
2026-07-21 15:51:31 +0800

    refactor: 暂估应付单提交改为保存操作服务插件，新增提示功能

---
+++ 益讯机械/暂估应付单保存更新付款计划行号.py
@@ -0,0 +1,48 @@
+import clr
+clr.AddReference('System')
+clr.AddReference('System.Data')
+clr.AddReference('Kingdee.BOS')
+clr.AddReference('Kingdee.BOS.DataEntity')
+clr.AddReference('Kingdee.BOS.Core')
+clr.AddReference('Kingdee.BOS.ServiceHelper')
+
+from Kingdee.BOS import *
+from Kingdee.BOS.Core import *
+from Kingdee.BOS.Orm.DataEntity import *
+from Kingdee.BOS.ServiceHelper import DBServiceHelper
+from System import *
+from System.Data import *
+
+
+def OnPreparePropertys(e):
+	e.FieldKeys.Add("FSETACCOUNTTYPE")
+	e.FieldKeys.Add("AP_PAYABLEPLAN")
+	e.FieldKeys.Add("AP_PAYABLEENTRY")
+
+
+def EndOperationTransaction(e):
+	for billObj in e.DataEntitys:
+		acctType = str(billObj["FSETACCOUNTTYPE"]) if billObj["FSETACCOUNTTYPE"] is not None else ""
+		if acctType != "2":
+			continue
+
+		payPlan = billObj["AP_PAYABLEPLAN"]
+		if payPlan is None or payPlan.Count == 0:
+			continue
+
+		entries = billObj["AP_PAYABLEENTRY"]
+		if entries is None or entries.Count == 0:
+			continue
+
+		billId = Convert.ToInt64(billObj["Id"])
+		firstEntryId = Convert.ToInt64(entries[0]["Id"])
+
+		sql = ("/*dialect*/ UPDATE T_AP_PAYABLEPLAN "
+			"SET FENTRYID={0},FSEQ='1' "
+			"WHERE FID={1} AND (FENTRYID IS NULL OR FENTRYID=0)").format(firstEntryId, billId)
+		DBServiceHelper.ExecuteDataSet(this.Context, sql)
+
+		result = OperateResult()
+		result.SuccessStatus = True
+		result.Message = "付款计划行号更新成功"
+		this.OperationResult.OperateResult.Add(result)
